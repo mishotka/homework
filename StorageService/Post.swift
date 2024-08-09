826d6cf59d22dfc250668b12451c9c49029c7e3c
mishotka <mixailgusew@yandex.ru>
2024-08-09 23:39:00 +0300

    first commit

---
+++ StorageService/Post.swift
@@ -0,0 +1,23 @@
+//
+//  Post.swift
+//  Navigation
+//
+
+import Foundation
+
+public struct Post {
+    public var author: String
+    public var description: String
+    public var image: String
+    public var likes: Int
+    public var views: Int
+    
+    public init(author: String, description: String, image: String, likes: Int, views: Int) {
+        self.author = author
+        self.description = description
+        self.image = image
+        self.likes = likes
+        self.views = views
+    }
+}
+
